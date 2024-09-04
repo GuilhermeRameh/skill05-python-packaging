e5ecb0f8ca9675caf7a9a8cd4cd7572269da052b
GuilhermeRameh <guilhermedr@al.insper.edu.br>
2024-09-04 15:38:46 -0300

    Finalizada atividade, disponivel por pip

---
+++ build/scripts-3.12/hello.py
@@ -0,0 +1,6 @@
+#!python
+from dev_aberto import hello
+
+if __name__ == '__main__':
+    date, name = hello()
+    print('Último commit feito em:', date, ' por', name)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dev_aberto_guilhermedr',
+    version='0.1',
+    packages=['dev_aberto'],
+    author='Guilherme Rameh',
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+    ],
+    scripts=['scripts/hello.py'],
+)
